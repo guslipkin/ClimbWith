e78d05d27e376dd844c568ec82cddadcf11cb2e7
guslipkin <gus.lipkin@gmail.com>
2024-04-11 19:28:04 -0400

    fix boards

--- data-raw/external-data.R
+++ data-raw/external-data.R
@@ -1,3 +1,9 @@
 googlesheets4::gs4_deauth()
 googlesheets4::read_sheet('1h1hfs-GXo9-9CnAKM25Rd3aagBQcE4pkzDRu9F0_fHA', sheet = 'Validated') |>
+  dplyr::mutate(
+    dplyr::across(
+      tidyselect::where(is.list),
+      \(x) sapply(x, \(y) ifelse(is.null(y) | isTRUE(y == 'NULL'), NA, y))
+    )
+  ) |>
   readr::write_csv('data-raw/data.csv')
